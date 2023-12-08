becb1bbf9e84cdfcba0a5ab649481e8c3c22b4e8
Erensu Deveci <erensudeveci@gmail.com>
2023-12-08 14:17:38 -0800

    Delete fidye_virusu.py

--- fidye_virusu.py
+++
@@ -1,9 +0,0 @@
-import os
-import socket
-import getpass
-from cryptography.fernet import Fernet
-
-sock = socket.socket()
-sock.connect(("10.0.2.4",8080))
-sock.send("Merhaba".encode("utf-8"))
-sock.close()
